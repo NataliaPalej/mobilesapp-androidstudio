762f4cfc1f7571a42fce7321e0607397f8a53596
Natalia Palej <palejnatalia@gmail.com>
2023-02-23 12:52:59 +0000

    Declared radio buttons and added back button function

--- app/src/main/java/com/example/myapplication/BeachScreen.java
+++ app/src/main/java/com/example/myapplication/BeachScreen.java
@@ -5,10 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class BeachScreen extends AppCompatActivity {
 
-    private Button exit;
+    //Back button
+    private Button btn;
+
+    // Radio buttons for different beaches
+    private RadioButton radio1;
+    private RadioButton radio2;
+    private RadioButton radio3;
+    private RadioButton radio4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +25,8 @@ public class BeachScreen extends AppCompatActivity {
         setContentView(R.layout.activity_beach_screen);
 
         // Go to MenuScreen
-        exit = findViewById(R.id.btn);
-        exit.setOnClickListener(new View.OnClickListener() {
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(BeachScreen.this, MenuScreen.class);
