24671465ee847c8c5986013093ac7a68a92dbca6
Natalia Palej <palejnatalia@gmail.com>
2023-02-23 12:51:04 +0000

    Added radio buttons to BeachScreen

--- app/src/main/java/com/example/myapplication/BeachScreen.java
+++ app/src/main/java/com/example/myapplication/BeachScreen.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BeachScreen extends AppCompatActivity {
+
+    private Button exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beach_screen);
+
+        // Go to MenuScreen
+        exit = findViewById(R.id.btn);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(BeachScreen.this, MenuScreen.class);
+                startActivity(i);
+            }
+        });
     }
 }
