289c21d3a088b9ad3a206d375872fdd15eeb0cda
Natalia Palej <palejnatalia@gmail.com>
2023-02-23 12:30:35 +0000

    Created screens and added functionallity to MenuScreen

---
+++ app/src/main/java/com/example/myapplication/AdventureScreen.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdventureScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adventure_screen);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MenuScreen.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuScreen extends AppCompatActivity {
+
+    private Button adventureScreen;
+    private Button beachScreen;
+    private Button hotelScreen;
+    private Button transportScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_screen);
+
+        // Go to Transport Screen
+        transportScreen = findViewById(R.id.btn2);
+        transportScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuScreen.this, TransportScreen.class);
+                startActivity(i);
+            }
+        });
+
+        // Go to Hotel Screen
+        hotelScreen = findViewById(R.id.btn2);
+        hotelScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuScreen.this, HotelsScreen.class);
+                startActivity(i);
+            }
+        });
+
+        // Go to Adventure Screen
+        adventureScreen = findViewById(R.id.btn3);
+        adventureScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuScreen.this, AdventureScreen.class);
+                startActivity(i);
+            }
+        });
+
+        // Go to Beach Screen
+        beachScreen = findViewById(R.id.btn4);
+        beachScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MenuScreen.this, BeachScreen.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
