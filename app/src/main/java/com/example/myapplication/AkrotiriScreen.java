e56eff574f602eef72f0e5d2978e4d18fb0fed0d
Natalia Palej <palejnatalia@gmail.com>
2023-02-25 22:05:29 +0000

    Implemented OiaScreen, added hotel hyperlinks

--- app/src/main/java/com/example/myapplication/AkrotiriScreen.java
+++ app/src/main/java/com/example/myapplication/AkrotiriScreen.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -18,24 +17,6 @@ public class AkrotiriScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_akrotiri_screen);
-
-        menu = findViewById(R.id.menu1);
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(AkrotiriScreen.this, MenuScreen.class);
-                startActivity(i);
-            }
-        });
-
-        previous = findViewById(R.id.backbtn2);
-        previous.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(AkrotiriScreen.this, OiaScreen.class);
-                startActivity(i);
-            }
-        });
 
 
     }
--- app/src/main/java/com/example/myapplication/OiaScreen.java
+++ app/src/main/java/com/example/myapplication/OiaScreen.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,13 +15,15 @@ public class OiaScreen extends AppCompatActivity {
     private ImageView previous;
     private ImageView next;
     private Button trip;
+    private Button hoteldelta, hotelpersa, hoteloiamare, hotelsecret;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_akrotiri_screen);
 
-        next = findViewById(R.id.nextbtn2);
+        next = findViewById(R.id.next2);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,8 +32,8 @@ public class OiaScreen extends AppCompatActivity {
             }
         });
 
-        previous = findViewById(R.id.backbtn2);
-        previous.setOnClickListener(new View.OnClickListener() {
+        next = findViewById(R.id.back2);
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(OiaScreen.this, FiraScreen.class);
@@ -38,6 +41,44 @@ public class OiaScreen extends AppCompatActivity {
             }
         });
 
+        hoteldelta = findViewById(R.id.hoteldelta_btn);
+        hoteldelta.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://tinyurl.com/2x9ap3y5");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        hoteloiamare = findViewById(R.id.hoteloiamare_btn);
+        hoteloiamare.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://tinyurl.com/3rfthr98");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        hotelpersa = findViewById(R.id.hotelpersa_btn);
+        hotelpersa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://tinyurl.com/pavdp8eu");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        hotelsecret = findViewById(R.id.hotelsecret_btn);
+        hotelsecret.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://tinyurl.com/2dhhexuy");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        
         trip = findViewById(R.id.firaoiatrip);
         trip.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -46,4 +87,5 @@ public class OiaScreen extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 }
