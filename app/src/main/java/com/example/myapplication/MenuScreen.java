e9259776370f52ff402651c8f10e43ff0fdc0727
Natalia Palej <palejnatalia@gmail.com>
2023-03-07 12:04:29 +0000

    Perform actions when specific menu button selected

--- app/src/main/java/com/example/myapplication/MenuScreen.java
+++ app/src/main/java/com/example/myapplication/MenuScreen.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -76,6 +78,47 @@ public class MenuScreen extends AppCompatActivity {
                 System.exit(0);
             }
         });
+    }
 
+    // Show menu tab on first screen
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity, menu);
+        return true;
+    }
+
+    // Perform action when menu item selected
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.mapMenu) {
+            Intent i = new Intent(MenuScreen.this, MapScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.mainMenu) {
+            Intent i = new Intent(MenuScreen.this, MenuScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.transportMenu) {
+            Intent i = new Intent(MenuScreen.this, TransportScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.beachMenu) {
+            Intent i = new Intent(MenuScreen.this, BeachScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.tipsMenu) {
+            Intent i = new Intent(MenuScreen.this, TipsScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.adventureMenu) {
+            Intent i = new Intent(MenuScreen.this, AdventureScreen.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
