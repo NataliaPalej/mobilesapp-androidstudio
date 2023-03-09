dcdc1140e9964146fc6135a629c066d6befaa6ed
Natalia Palej <palejnatalia@gmail.com>
2023-03-09 16:32:35 +0000

    Fixed check-in list in FiraScreen

--- app/src/main/java/com/example/myapplication/FiraScreen.java
+++ app/src/main/java/com/example/myapplication/FiraScreen.java
@@ -51,37 +51,25 @@ public class FiraScreen extends AppCompatActivity {
                 if(count==3){
                     firaPictures.setImageResource(R.drawable.fira3);
                 }
-                if(count==3){
+                if(count==4){
                     firaPictures.setImageResource(R.drawable.fira4);
                 }
-                if(count==3){
+                if(count==5){
                     firaPictures.setImageResource(R.drawable.fira5);
                 }
-                if(count==3){
+                if(count==6){
                     firaPictures.setImageResource(R.drawable.fira6);
                 }
-                if(count==3){
+                if(count==7){
                     firaPictures.setImageResource(R.drawable.fira7);
                 }
-                if(count==3){
+                if(count==8){
                     firaPictures.setImageResource(R.drawable.fira8);
                 }
-                if(count==3){
+                if(count==9){
                     firaPictures.setImageResource(R.drawable.fira9);
                     count=0;
                 }
-            }
-        });
-
-        // Go to Fira on maps
-        firaMap = findViewById(R.id.firaMap);
-        firaMap.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent i = new Intent();
-                i.setAction(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("https://www.google.com/maps/search/fira+main+square/@36.4185485,25.4303848,693m/data=!3m2!1e3!4b1"));
-                startActivity(i);
             }
         });
     }
