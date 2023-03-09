e4b572a39851e8e0a16186c9e039ef75f1aa7160
Natalia Palej <palejnatalia@gmail.com>
2023-03-09 16:50:08 +0000

    Added MenuTab to every screen

--- app/src/main/java/com/example/myapplication/SignUpScreen.java
+++ app/src/main/java/com/example/myapplication/SignUpScreen.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SignUpScreen extends AppCompatActivity {
 
@@ -10,5 +13,47 @@ public class SignUpScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
+    }
+
+    // Show menu tab on first screen
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity, menu);
+        return true;
+    }
+
+    // Perform action when menu item selected
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.mapMenu) {
+            Intent i = new Intent(SignUpScreen.this, MapScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.mainMenu) {
+            Intent i = new Intent(SignUpScreen.this, MenuScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.transportMenu) {
+            Intent i = new Intent(SignUpScreen.this, TransportScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.beachMenu) {
+            Intent i = new Intent(SignUpScreen.this, BeachScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.tipsMenu) {
+            Intent i = new Intent(SignUpScreen.this, TipsScreen.class);
+            startActivity(i);
+            return true;
+        }
+        if (id == R.id.adventureMenu) {
+            Intent i = new Intent(SignUpScreen.this, AdventureScreen.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
