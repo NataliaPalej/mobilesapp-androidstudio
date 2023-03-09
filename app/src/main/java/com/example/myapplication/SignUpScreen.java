74b25155189f20231ea70520ad39824f72b25252
Natalia Palej <palejnatalia@gmail.com>
2023-03-09 16:56:31 +0000

    Started implementing SMS and Email functionallity

--- app/src/main/java/com/example/myapplication/SignUpScreen.java
+++ app/src/main/java/com/example/myapplication/SignUpScreen.java
@@ -3,16 +3,47 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignUpScreen extends AppCompatActivity {
+
+    // TODO -> create id_email and id_sms image view on activity_sign_up_screen
+    private ImageView id_email, id_sms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
+
+        id_email = findViewById(R.id.id_email);
+        id_sms = findViewById(R.id.id_sms);
+
+        id_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{"tom@ait.ie"});
+                email.putExtra(Intent.EXTRA_SUBJECT, "Hello from my app");
+                email.putExtra(Intent.EXTRA_TEXT, "Bla bla Bla bla  Bla bla  Bla bla  Bla bla ");
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email, "Send Mail Using :"));
+            }
+        });
+
+        id_sms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("smsto:2345612345");
+                Intent msg = new Intent(Intent.ACTION_SENDTO, uri);
+                msg.putExtra("sms_body", "The SMS text");
+                startActivity(Intent.createChooser(msg, "Send sms Using :"));
+            }
+        });
     }
 
     // Show menu tab on first screen
