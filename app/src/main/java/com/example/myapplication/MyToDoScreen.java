d45fc641c1752d2a5fe6d23021abd350de9fd9e6
Natalia Palej <palejnatalia@gmail.com>
2023-03-14 12:16:31 +0000

    MyToDo list successfuly saving to database

--- app/src/main/java/com/example/myapplication/MyToDoScreen.java
+++ app/src/main/java/com/example/myapplication/MyToDoScreen.java
@@ -3,16 +3,50 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MyToDoScreen extends AppCompatActivity {
+
+    private Button saveDetails, showDetails;
+    private TextView todoDetails;
+    private SharedPreferences database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_to_do_screen);
+
+        saveDetails = findViewById(R.id.saveDetails);
+        showDetails = findViewById(R.id.showDetails);
+        todoDetails = findViewById(R.id.todoDetails);
+
+        saveDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                database = getApplicationContext().getSharedPreferences("table_store_text", MODE_PRIVATE);
+                SharedPreferences.Editor editor = database.edit();
+                String todoList = todoDetails.getText().toString();
+                editor.putString("key_saved_text", todoList);
+                editor.commit();
+                Toast.makeText(MyToDoScreen.this, "Data Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        showDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database = getApplicationContext().getSharedPreferences("table_store_text", MODE_PRIVATE);
+                String todoList = database.getString("key_saved_text", null);
+                Toast.makeText(MyToDoScreen.this, todoList, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     // Show menu tab on screen
