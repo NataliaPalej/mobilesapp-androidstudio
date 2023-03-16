a0c6ecdbb846e2c5e4dc480c2923cc04058cdf9f
Natalia Palej <palejnatalia@gmail.com>
2023-03-16 20:01:14 +0000

    Finished SimpleListView on RestaurantScreen

--- app/src/main/java/com/example/myapplication/RestaurantsScreen.java
+++ app/src/main/java/com/example/myapplication/RestaurantsScreen.java
@@ -6,19 +6,63 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-// TODO simple list to be implemented
-// https://www.youtube.com/watch?v=gGCkHopziSg&ab_channel=2023-Android
+import java.util.ArrayList;
 
 public class RestaurantsScreen extends AppCompatActivity {
 
-    private Button backBtn11;
+    private ImageView backbtn1;
+    private ListView listView;
+    private ArrayList<String> restaurantList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants_screen);
+
+        // Back Button
+        backbtn1 = findViewById(R.id.backbtn1);
+        backbtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RestaurantsScreen.this, MenuScreen.class);
+                startActivity(i);
+            }
+        });
+
+        // TODO: In 3rd App to enhance the listView, restaurant price and rating will display on click
+        // Create List View
+        listView = findViewById(R.id.listView);
+        restaurantList = new ArrayList<>();
+        restaurantList.add("Fanari · €€ · Rating: 4.4");
+        restaurantList.add("Volcano Blue · €€€ · Rating: 4.4");
+        restaurantList.add("Idol Restaurant Bar · €€€ · Rating: 4.2");
+        restaurantList.add("Da Costa · €€ · Rating: 4.3");
+        restaurantList.add("La Scala · €€ · Rating: 4.2");
+        restaurantList.add("Aris Restaurants · unknown · Rating: 4.7");
+        restaurantList.add("La Colline · unknown · Rating: 4.7");
+        restaurantList.add("Lithos · €€ · Rating: 4.0");
+        restaurantList.add("Zafora · €€ · Rating: 3.1");
+
+        ArrayAdapter<String> restaurantAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                restaurantList
+        );
+
+        listView.setAdapter(restaurantAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView < ? > adapterView, View view,int i, long l){
+                Toast.makeText(RestaurantsScreen.this, restaurantList.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     // Show menu tab on screen
